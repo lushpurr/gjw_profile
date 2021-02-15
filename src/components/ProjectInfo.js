52042250d886930177911bfdd3aaceb172f77590
lushpurr <lushpurr@gmail.com>
2021-02-15 22:24:29 +0000

    increased size on computer screen and css changes for phone screen

--- src/components/ProjectInfo.js
+++ src/components/ProjectInfo.js
@@ -27,15 +27,15 @@ const ProjectInfo = () => {
         <h3>Portfolio - Pick a project for more info</h3> 
         <span>
             <div id="project-info">
-                <img className="project-image" src={Face}/>
+                <img className="project-face" src={Face}/>
                 <ul>
                     <li>Languages: Javascript, Python, Java</li>
                     <li>Frontend Frameworks: React, Vue.js, Flask</li>
                     <li>Other Technologies: IntelliJ, Node.js, MongoDB, Git, Github, Html5, CSS, Mocha, Express.js, Spring, PostgreSQL</li>
                     <li id="blank-li"></li>
-                    <li className={emailClass} data-clipboard-text="gavinjwill@gmail.com"><FaEnvelope/>Email</li>
-                    <li className="contact-details"><a href="https://www.linkedin.com/in/gavin-j-will"><FaLinkedin/>LinkedIn</a></li>
-                    <li className="contact-details"><a href="https://github.com/lushpurr"><FaGithub/>Github</a></li>
+                    <li className={emailClass} data-clipboard-text="gavinjwill@gmail.com"><FaEnvelope className="icon" />Email</li>
+                    <li className="contact-details"><a href="https://www.linkedin.com/in/gavin-j-will"><FaLinkedin className="icon" />LinkedIn</a></li>
+                    <li className="contact-details"><a href="https://github.com/lushpurr"><FaGithub className="icon" />Github</a></li>
                     
                 </ul>
             </div>
--- src/components/ProjectNav.js
+++ src/components/ProjectNav.js
@@ -11,22 +11,22 @@ function ProjectNav(){
 
             <ul className='nav'>
                 <li>
-                    <NavLink exact to={process.env.PUBLIC_URL + '/'}><FaIdCard className="icon"/></NavLink>
+                    <NavLink exact to={process.env.PUBLIC_URL + '/'}><FaIdCard className="icon" title="Profile"/></NavLink>
                 </li>
                 <li>
-                    <NavLink to="/spend-tracker"><FaMoneyBillWave className="icon" alt="Spend Tracker"/></NavLink>
+                    <NavLink to="/spend-tracker"><FaMoneyBillWave className="icon" title="Spend Tracker"/></NavLink>
                 </li>
                 <li>
-                    <NavLink to="/solar-system"><FaMoon className="icon"/></NavLink>
+                    <NavLink to="/solar-system"><FaMoon className="icon" title="Solar System App"/></NavLink>
                 </li>
                 <li> 
-                    <NavLink to="/food-management"><FaPizzaSlice className="icon"/></NavLink>
+                    <NavLink to="/food-management"><FaPizzaSlice className="icon" title="Restaurant Ordering Management App"/></NavLink>
                 </li>
                 <li> 
-                    <NavLink to="/music"><FaHeadphones className="icon"/></NavLink>
+                    <NavLink to="/music"><FaHeadphones className="icon" title="Music Artist Website"/></NavLink>
                 </li>
                 <li> 
-                    <NavLink to="/adventure-game"><FaDungeon className="icon"/></NavLink>
+                    <NavLink to="/adventure-game"><FaDungeon className="icon" title="Text Adventure Game" /></NavLink>
                 </li>
             </ul>
         </nav>
