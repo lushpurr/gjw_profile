47dda348839c44aa6627de158c0ddcc068e8fba0
lushpurr <lushpurr@gmail.com>
2021-02-15 18:34:05 +0000

    added contact icons and email clipboard copy

--- src/components/ProjectInfo.js
+++ src/components/ProjectInfo.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import Face from '.././images/face.jpg';
+import Clipboard from "clipboard";
+import classNames from 'classnames';
+
+import {FaEnvelope ,FaGithub, FaLinkedin} from 'react-icons/fa';
+
 import '../components/projects/Projects.css';
+
+const clip = new Clipboard(".btn");
+
+clip.on("success", function(){
+    alert("Email address copied to clipboard.");
+});
+
+clip.on("error", function(){
+    alert("Cant' copy email address to clipboard, sorry");
+});
+
+const emailClass = classNames('btn', 'contact-details');
 
 const ProjectInfo = () => {
 
@@ -16,9 +33,10 @@ const ProjectInfo = () => {
                     <li>Frontend Frameworks: React, Vue.js, Flask</li>
                     <li>Other Technologies: IntelliJ, Node.js, MongoDB, Git, Github, Html5, CSS, Mocha, Express.js, Spring, PostgreSQL</li>
                     <li id="blank-li"></li>
-                    <li className="contact-details">gavinjwill@gmail.com</li>
-                    <li className="contact-details"><a href="https://www.linkedin.com/in/gavin-j-will">LinkedIn</a></li>
-                    <li className="contact-details"><a href="https://github.com/lushpurr">Github</a></li>
+                    <li className={emailClass} data-clipboard-text="gavinjwill@gmail.com"><FaEnvelope/>Email</li>
+                    <li className="contact-details"><a href="https://www.linkedin.com/in/gavin-j-will"><FaLinkedin/>LinkedIn</a></li>
+                    <li className="contact-details"><a href="https://github.com/lushpurr"><FaGithub/>Github</a></li>
+                    
                 </ul>
             </div>
         </span>
