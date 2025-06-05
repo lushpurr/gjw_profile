35c9a421c683108be4fa0a8809c9b5c4bd468596
Gavin Will <gavinjwill@gmail.com>
2025-06-05 11:33:02 +0100

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import '../components/projects/Projects.css';
 const Footer = () => {
     return (
         <>
-        <p>Made with React | Gavin J Will | 2021</p>
+        <p>Made with React | Gavin J Will | 2025</p>
         </>
     )
 }
